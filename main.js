94864b5df3b01afd8b83ac75493a1cc805bbee25
Islomjon0221 <islomjon20110221@gmail.com>
2023-12-24 21:15:31 +0500

    Qo'shimcha vazifa ham qilindi

--- main.js
+++ main.js
@@ -50,14 +50,85 @@
 
 // 5-misol
 
-function mashq5 (arg) {
-    let sum5 = 1;
-    for (let i = 1; i <= arg; i++) {
-        if (i % 2 == 1) {
-            sum5 *= i;
-        }
-    }
-    return sum5
-}
+// function mashq5 (arg) {
+//     let sum5 = 1;
+//     for (let i = 1; i <= arg; i++) {
+//         if (i % 2 == 1) {
+//             sum5 *= i;
+//         }
+//     }
+//     return sum5
+// }
 
-console.log(mashq5(5));
+// console.log(mashq5(5));
+
+// 1-error
+
+// function TypeError1 (a, b) {
+//     if (typeof a != Number && typeof b != Number) {
+//         throw new TypeError("Berilgan qilmat son emas")
+//     }
+//     return a + b
+// }
+
+
+// function TypeError2 (a, b) {
+//     if (typeof a != Number && typeof b != Number) {
+//         throw new TypeError("Berilgan qilmat son emas")
+//     }
+//     return a - b
+// }
+
+// console.log(TypeError1(2, 2));
+// console.log(TypeError2(2, 2));
+
+// 2-error
+
+// function ReferenceError1 (a, b) {
+//     res = test + b
+//     if (res != a + b) {
+//         throw new ReferenceError("Bunday o'zgaruvchi topilmadi")
+//     }
+// }
+
+// console.log(ReferenceError1(1, 2));
+
+
+// function ReferenceError2 (a, b) {
+//     res = test - b
+//     if (res != a - b) {
+//         throw new ReferenceError("Bunday o'zgaruvchi topilmadi")
+//     }
+// }
+
+// console.log(ReferenceError2(1, 2));
+
+// 3-error
+
+// function RangeError1 (a) {
+//     if (a > 2023) {
+//         throw new RangeError ("Bunday yil yo'q")
+//     }
+
+//     if (a < 1850) {
+//         throw new RangeError ("Bunday yosh yo'q")
+//     }
+
+//     return 2023 - a;
+// }
+
+// console.log(RangeError1(1900));
+
+// function RangeError1 (a) {
+//     if (a > 150) {
+//         throw new RangeError ("Bunday yosh yo'q")
+//     }
+
+//     if (a < 0) {
+//         throw new RangeError ("Bunday yil yo'q")
+//     }
+
+//     return 2023 - a;
+// }
+
+// console.log(RangeError1(1));
